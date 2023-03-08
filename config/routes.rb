62b67717a52f5fbec2f9c28c51e7c59a5feba7e2
mitsunobu1201 <87081727+mitsunobu1201@users.noreply.github.com>
2023-03-08 11:45:14 +0000

    [Add]検索機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  #フォロー、フォロワー
   get 'relationships/followings'
   get 'relationships/followers'
-  
+
+  #検索
+  get "search" => "searches#search"
+
 
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
@@ -11,11 +15,11 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create,:destroy]
     resources :book_comments, only: [:create,:destroy]
   end
-  
-  resources :users, only: [:index,:show,:edit,:update] do 
+
+  resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
-  
+
 end
